10323dd8fe8435c332a9e5eac0104265533ce9a7
Patilshivam <35370465+Patilshivam@users.noreply.github.com>
2024-10-06 13:19:26 +0530

    Create create_delta_table.py

---
+++ create_delta_table.py
@@ -0,0 +1,10 @@
+from pyspark.sql.types import *
+
+dt1 = (
+    DeltaTable.create(spark)
+    .tableName("testTable1")
+    .addColumn("c1", dataType="INT", nullable=False)
+    .addColumn("c2", dataType=IntegerType(), generatedAlwaysAs="c1 + 1")
+    .partitionedBy("c1")
+    .execute()
+)
