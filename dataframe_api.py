a4ba2455df2de1eec7e827c0fabe11cdcd1ffbb5
poojapatilxxi <94454580+poojapatilxxi@users.noreply.github.com>
2024-10-06 13:44:47 +0530

    Update dataframe_api.py

--- dataframe_api.py
+++ dataframe_api.py
@@ -1,5 +1,5 @@
 data = [[2021, "test", "Albany", "M", 42]]
 columns = ["Year", "First_Name", "County", "Sex", "Count"]
 
-df1 = spark.createDataFrame(data, schema="Year int, First_Name STRING, County STRING, Sex STRING, Count int")
-display(df1)
+df_res = spark.createDataFrame(data, schema="Year int, First_Name STRING, County STRING, Sex STRING, Count int")
+display(df_res)
