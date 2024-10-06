b033d78d15241e40045c6f94eb093a16a38170fc
poojapatilxxi <94454580+poojapatilxxi@users.noreply.github.com>
2024-10-06 13:51:08 +0530

    Update create_delta_table.py

--- create_delta_table.py
+++ create_delta_table.py
@@ -1,10 +1,13 @@
+#Import statement
 from pyspark.sql.types import *
+
 #create table statement
-dt1 = (
+dt_var = (
     DeltaTable.create(spark)
     .tableName("testTable1")
     .addColumn("c1", dataType="INT", nullable=False)
     .addColumn("c2", dataType=IntegerType(), generatedAlwaysAs="c1 + 1")
     .partitionedBy("c1")
     .execute()
+    .show()
 )
